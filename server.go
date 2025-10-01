283321e619a4b0e2a6c65a0a9d0df84b79e06a5b
wtc <kxcr777@qq.com>
2025-10-01 12:25:52 +0800

    基础Server搭建与用户上线及广播通知

---
+++ server.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+// 创建服务器类
+type Server struct {
+	Ip   string
+	Port int
+
+	OnlineMap map[string]*User //服务器的用户列表
+	mapLock   sync.RWMutex     //全局锁
+	Message   chan string      // 服务器的管道
+
+}
+
+// 提供一个获得Server的接口
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
+	return server
+}
+
+// 服务器channel一旦有消息就广播发送该消息
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+		//一旦有消息发送给全部的在线用户
+		this.mapLock.Lock()
+		for _, value := range this.OnlineMap {
+			value.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+// 广播发送用户上线
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+
+}
+
+// 处理当前连接的业务handler
+func (this *Server) Handler(conn net.Conn) {
+	fmt.Println("连接建立成功！！！")
+
+	//将新增加的用户添加到全局用户列表中
+	user := NewUser(conn)
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	//广播当前用户上线的方法进行通知
+	this.BroadCast(user, "已上线")
+
+	//当前handle阻塞
+	select {}
+}
+
+// 给Server类添加一个启动的方法（该方法为成员方法）
+func (this *Server) Start() {
+	// 1 : 进行socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listener err is", err)
+		return
+	}
+	defer listener.Close()
+
+	//启动监听消息的goroutine
+	go this.ListenMessager()
+
+	for {
+		// accept 进行连接
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept error:", err)
+		}
+		//do hanlder
+		go this.Handler(conn)
+	}
+}
